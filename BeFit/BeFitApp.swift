dbff59ee7aa4597946f8541fd468f49d1e741f82
graceco22 <132722611+graceco22@users.noreply.github.com>
2024-04-06 12:05:01 -0700

    Initial Commit

---
+++ BeFit/BeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitApp.swift
+//  BeFit
+//
+//  Created by Grace Co on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct BeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
